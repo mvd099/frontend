a84e347335ea9d68b86597cb19fe55bf0fe37d95
carvalh8 <carvalh8@coventry.ac.uk>
2022-09-25 22:20:04 +0100

    Add Gender, First and Last Name and Resgister Attendance

---
+++ Scripts/script.js
@@ -0,0 +1,11 @@
+function openForm() {
+    document.getElementById("myForm").style.display = "block";
+    document.getElementById("myDetails").style.display = "none";
+    document.getElementById("openButton").style.display = "none";
+  }
+  
+  function closeForm() {
+    document.getElementById("myForm").style.display = "none";
+    document.getElementById("myDetails").style.display = "block";
+    document.getElementById("openButton").style.display = "block";
+  }
